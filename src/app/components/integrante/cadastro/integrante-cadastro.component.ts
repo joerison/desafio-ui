df82d0f4e02c325717cee91ba42ff18061d9c94c
Joerison Silva <joerison@gmail.com>
2025-05-06 21:56:54 -0300

    Finalizado cadastro de integrantes.

--- src/app/components/integrante/cadastro/integrante-cadastro.component.ts
+++ src/app/components/integrante/cadastro/integrante-cadastro.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Integrante, IntegranteService} from '../integrante.service';
 import {Card} from 'primeng/card';
 import {catchError} from 'rxjs';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {NgIf} from '@angular/common';
+import {JsonPipe, NgIf} from '@angular/common';
 import {InputText} from 'primeng/inputtext';
 import {Select} from 'primeng/select';
 import {FranquiaService} from '../../franquia/franquia.service';
@@ -14,92 +14,104 @@ import {IconField} from 'primeng/iconfield';
 import {InputIcon} from 'primeng/inputicon';
 
 @Component({
-  selector: 'app-integrante-cadastro',
-  templateUrl: './integrante-cadastro.component.html',
-  styleUrls: ['./integrante-cadastro.component.css'],
-  imports: [
-    Card,
-    ReactiveFormsModule,
-    InputText,
-    NgIf,
-    Select,
-    Button,
-    Textarea,
-    Fluid,
-    IconField,
-    InputIcon
-  ],
-  providers: [IntegranteService, FranquiaService],
+    selector: 'app-integrante-cadastro',
+    templateUrl: './integrante-cadastro.component.html',
+    styleUrls: ['./integrante-cadastro.component.css'],
+    imports: [
+        Card,
+        ReactiveFormsModule,
+        InputText,
+        NgIf,
+        Select,
+        Button,
+        Textarea,
+        Fluid,
+        IconField,
+        InputIcon,
+        JsonPipe
+    ],
+    providers: [IntegranteService, FranquiaService],
 })
 
 export class IntegranteCadastroComponent implements OnInit {
 
-  formulario: FormGroup;
-  enviado = false;
-  salvando = false;
-  franquias: any[] | undefined;
-  funcoes: any[] | undefined;
+    formulario: FormGroup;
+    enviado = false;
+    salvando = false;
+    franquias: any[] | undefined;
+    funcoes: any[] | undefined;
 
-  constructor(private integranteService: IntegranteService,
-              private franquiaService: FranquiaService,
-              private formBuilder: FormBuilder) {
+    @Output()
+    integranteCadastradoEvent: EventEmitter<any> = new EventEmitter();
 
-    this.formulario = this.formBuilder.group({
-      id: [''],
-      nome: ['', Validators.required],
-      funcao: this.formBuilder.group({id: [''],
-        franquia: this.formBuilder.group({id: ['']})
-      }),
-    });
+    constructor(private integranteService: IntegranteService,
+                private franquiaService: FranquiaService,
+                private formBuilder: FormBuilder) {
 
-    this.franquiaService.listar().subscribe(franquias => this.franquias = franquias);
+        this.formulario = this.formBuilder.group({
+            id: [''],
+            nome: ['', Validators.required],
+            funcao: this.formBuilder.group({
+                id: ['', Validators.required],
+                franquia: this.formBuilder.group({
+                    id: ['', Validators.required]
+                })
+            }),
+        });
 
-    this.formulario.get('funcao')?.get('franquia')?.valueChanges?.subscribe(franquia => {
-      if (franquia?.id) {
-        this.franquiaService.listarFuncoes(franquia?.id).subscribe(funcoes => this.funcoes = funcoes);
-      }else{
-        this.funcoes = [];
-      }
-    })
+        this.franquiaService.listar().subscribe(franquias => this.franquias = franquias);
 
-  }
+        this.formulario.get('funcao')?.get('franquia')?.valueChanges?.subscribe(franquia => {
+            if (franquia?.id) {
+                this.franquiaService.listarFuncoes(franquia?.id).subscribe(funcoes => this.funcoes = funcoes);
+            }else{
+                this.funcoes = [];
+            }
+        })
 
-  ngOnInit(): void {
-
-  }
-
-  isCampoInvalido(campo: string){
-    return (this.formulario?.get(campo)?.invalid &&
-      ((this.formulario?.get(campo)?.dirty || this.formulario?.get(campo)?.touched) || this.enviado));
-  }
-
-  salvar() {
-    this.enviado = true;
-
-    if (this.formulario?.invalid){
-      return
     }
 
-    let integrante: any = { ...this.formulario?.getRawValue() };
-    this.salvando = true;
-    this.salvarIntegrante(integrante)
+    ngOnInit(): void {
 
-  }
+    }
 
-  salvarIntegrante(integrante: Integrante){
-    this.integranteService.salvar(integrante)
-      .pipe(catchError(error => {
-        this.salvando = false;
-        console.error(error)
-        return error;
-      }))
-      .subscribe((data: any)=> {
-        this.formulario.reset();
-        this.salvando = false;
-        this.enviado = false;
-        console.log(data);
-      });
-  }
+    isCampoInvalido(path: string): boolean {
+        const control = this.formulario.get(path);
+        return !!(
+            control &&
+            control.invalid &&
+            (control.dirty || control.touched || this.enviado)
+        );
+    }
+
+    salvar() {
+        this.enviado = true;
+
+        if (this.formulario?.invalid){
+            return
+        }
+
+        let integrante: any = { ...this.formulario?.getRawValue() };
+        this.salvando = true;
+        this.salvarIntegrante(integrante)
+
+    }
+
+    salvarIntegrante(integrante: Integrante){
+        this.integranteService.salvar(integrante)
+            .pipe(catchError(error => {
+                this.salvando = false;
+                console.error(error)
+                return error;
+            }))
+            .subscribe((data: any)=> {
+                this.formulario.reset();
+                this.salvando = false;
+                this.enviado = false;
+                console.log(data);
+                this.integranteCadastradoEvent.emit();
+            });
+    }
 
 }
 
--- src/app/components/integrante/pesquisa/integrante-pesquisa.component.ts
+++ src/app/components/integrante/pesquisa/integrante-pesquisa.component.ts
@@ -6,41 +6,60 @@ import {TableModule} from 'primeng/table';
 import {Dialog} from 'primeng/dialog';
 import {IntegranteCadastroComponent} from '../cadastro/integrante-cadastro.component';
 import {Button} from 'primeng/button';
+import {Toast} from "primeng/toast";
+import {MessageService} from "primeng/api";
 
 @Component({
-  selector: 'app-integrante-pesquisa',
-  templateUrl: './integrante-pesquisa.component.html',
-  styleUrls: ['./integrante-pesquisa.component.css'],
-  imports: [
-    Card,
-    TableModule,
-    Dialog,
-    IntegranteCadastroComponent,
-    Button
-  ]
+    selector: 'app-integrante-pesquisa',
+    templateUrl: './integrante-pesquisa.component.html',
+    styleUrls: ['./integrante-pesquisa.component.css'],
+    providers: [MessageService],
+    imports: [
+        Card,
+        TableModule,
+        Dialog,
+        IntegranteCadastroComponent,
+        Button,
+        Toast
+    ]
 })
 
 export class IntegrantePesquisaComponent implements OnInit {
 
-  integrantes = [];
-  cadastroIntegranteDialog = false;
+    integrantes = [];
+    cadastroIntegranteDialog = false;
 
-  constructor(private IntegranteService: IntegranteService) {
-  }
+    constructor(private integranteService: IntegranteService,
+                private messageService: MessageService) {
+    }
 
-  ngOnInit(): void {
-    this.IntegranteService.listar()
-      .pipe(catchError(error => {
-        console.error(error);
-        return error;
-      })).subscribe(value => {
-      this.integrantes = value;
-    })
-  }
+    ngOnInit(): void {
+        this.listarIntegrantes();
+    }
 
-  abrirModalNovoIntegrante() {
-    this.cadastroIntegranteDialog = true;
-  }
+    abrirModalNovoIntegrante() {
+        this.cadastroIntegranteDialog = true;
+    }
+
+    listarIntegrantes() {
+        this.integranteService.listar()
+            .pipe(catchError(error => {
+                console.error(error);
+                return error;
+            })).subscribe(value => {
+            this.integrantes = value;
+        })
+    }
+
+    novoIntegranteCadastrado(){
+        this.listarIntegrantes();
+        this.messageService.add({
+            severity: 'success',
+            summary: 'Confirmação',
+            detail: 'Integrante salvo com sucesso!'
+        });
+        this.cadastroIntegranteDialog = false;
+    }
 
 }
 
